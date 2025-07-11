f85a2e76cf030f1379e76c90acd374815dd02aca
Orfeas Liossatos <oiliossatos@gmail.com>
2025-07-11 14:18:15 +0200

    add GLFW

---
+++ src/lib/glfw-3.4/examples/CMakeFiles/particles.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/orfeas/Projects/key-engine/src/lib/glfw-3.4/deps/getopt.c" "examples/CMakeFiles/particles.dir/__/deps/getopt.c.o" "gcc" "examples/CMakeFiles/particles.dir/__/deps/getopt.c.o.d"
+  "/home/orfeas/Projects/key-engine/src/lib/glfw-3.4/deps/tinycthread.c" "examples/CMakeFiles/particles.dir/__/deps/tinycthread.c.o" "gcc" "examples/CMakeFiles/particles.dir/__/deps/tinycthread.c.o.d"
+  "/home/orfeas/Projects/key-engine/src/lib/glfw-3.4/examples/particles.c" "examples/CMakeFiles/particles.dir/particles.c.o" "gcc" "examples/CMakeFiles/particles.dir/particles.c.o.d"
+  "" "examples/particles" "gcc" "examples/CMakeFiles/particles.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
